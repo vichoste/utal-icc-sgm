8556c9ecf5cf421177c9bc0c6d4937266fe492eb
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2022-12-26 17:12:47 -0300

    Disable userchangelimit mapping in the ORM

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+using Microsoft.AspNetCore.Identity;
 
 namespace Utal.Icc.Sgm.Models;
 
 public class ApplicationUser : IdentityUser {
 	public string? FirstName { get; set; }
 	public string? LastName { get; set; }
+	[NotMapped]
 	public int UsernameChangeLimit { get; set; } = 10;
 	public byte[]? ProfilePicture { get; set; }
 }
