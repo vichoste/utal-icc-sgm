dadf5a766b0847d57c39cb35e8965a85fef0751c
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2022-12-26 18:04:36 -0300

    Remove profile picture from applicationuser

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -7,7 +7,4 @@ namespace Utal.Icc.Sgm.Models;
 public class ApplicationUser : IdentityUser {
 	public string? FirstName { get; set; }
 	public string? LastName { get; set; }
-	[NotMapped]
-	public int UsernameChangeLimit { get; set; } = 10;
-	public byte[]? ProfilePicture { get; set; }
 }
