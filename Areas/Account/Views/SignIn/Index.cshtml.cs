d0b0967817104c96f2fb375e97af825e6fa80a45
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2023-01-05 19:53:30 -0300

    Login revamp

---
+++ Areas/Account/Views/SignIn/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Utal.Icc.Sgm.Areas.Account.Views.SignIn;
+
+public class IndexModel : PageModel {
+	[Display(Name = "E-mail"), EmailAddress, Required]
+	public string? Email { get; set; }
+	[Display(Name = "Contraseña"), Required]
+	public string? Password { get; set; }
+	[Display(Name = "Recuérdame")]
+	public bool RememberMe { get; set; }
+}
